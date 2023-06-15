381929d72f08a290142ea743811fec4fe8aae93a
Simran Preet Singh <simran5590@gmail.com>
2023-06-15 08:26:34 -0700

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Simran Preet Narang on 2023-06-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
